f025361463c738da1336c16e63771ba8171063bb
Johny <johny.ice10@gmail.com>
2020-12-20 17:48:27 +0600

    bug fix

--- app/src/main/java/com/example/guide/helper/Constant.java
+++ app/src/main/java/com/example/guide/helper/Constant.java
@@ -2,6 +2,6 @@ package com.example.guide.helper;
 
 public class Constant {
     public static String BASE_URL = "https://api.themoviedb.org/3/";
-    public static String API_KEY = "7c159687099060d09743c2fad3e844a4";
+    public static String API_KEY = "Place Your api key here...";
     public static String IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
 }
